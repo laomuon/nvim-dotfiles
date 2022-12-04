59b6a82d0c311f909dfc7a2c695757e4a7ca09fe
laomuon <laomuon@github.com>
2022-12-04 23:11:24 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Dotfile for neovim config
+
+Note: If on Ubuntu 18.04 or lower, build neovim from source
---
+++ init.lua
@@ -0,0 +1,115 @@
+vim.opt.guicursor = ""
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.errorbells = false
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.cursorline = true
+
+vim.opt.smartindent = true
+
+vim.cmd [[packadd packer.nvim]]
+
+require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+  
+  -- A file tree written in lua
+  use {
+  'nvim-tree/nvim-tree.lua',
+  requires = {
+    'nvim-tree/nvim-web-devicons', -- optional, for file icons
+  },
+  tag = 'nightly' -- optional, updated every week. (see issue #1193)
+  }
+
+  -- Gruvbox colortheme
+  use 'morhetz/gruvbox'
+ 
+  use{
+      'nvim-telescope/telescope.nvim',
+      requires = {{'nvim-lua/plenary.nvim'} }
+  }
+
+  use({
+        'nvim-lualine/lualine.nvim',
+      })
+  use("nvim-treesitter/nvim-treesitter", {
+        run = ":TSUpdate"
+    })
+  use("junegunn/fzf")
+  use("junegunn/fzf.vim")
+  use { 'neoclide/coc.nvim', branch='release' }
+end)
+
+-- Set colortheme
+vim.g.gruvbox_transparent_bg = '1'
+vim.g.gruvbox_invert_selection = '0'
+vim.g.gruvbox_contrast_dark = 'hard'
+
+vim.cmd("colorscheme gruvbox")
+
+require'nvim-treesitter.configs'.setup {
+    ensure_installed = "all",
+    sync_install = false,
+
+    highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+    },
+}
+
+require("lualine").setup({
+    sections = sections,
+    options = {
+        icons_enabled = true,
+    }
+})
+require('telescope').setup()
+require('nvim-tree').setup()
+
+vim.g.mapleader = " "
+
+local function bind(op, outer_opts)
+    outer_opts = outer_opts or {noremap = true}
+    return function(lhs, rhs, opts)
+        opts = vim.tbl_extend("force",
+            outer_opts,
+            opts or {}
+        )
+        vim.keymap.set(op, lhs, rhs, opts)
+    end
+end
+
+local nmap = bind("n", {noremap = false})
+local nnoremap = bind("n")
+local vnoremap = bind("v")
+local xnoremap = bind("x")
+local inoremap = bind("i")
+
+nnoremap("<leader>ft", ":NvimTreeToggle<CR>")
+nnoremap("<C-p>", ":Telescope")
+nnoremap("<leader>ps", function()
+    require('telescope.builtin').grep_string({ search = vim.fn.input("Grep For > ")})
+end)
+nnoremap("<C-p>", function()
+    require('telescope.builtin').git_files()
+end)
+nnoremap("<Leader>pf", function()
+    require('telescope.builtin').find_files()
+end)
+
+nnoremap("<leader>pw", function()
+    require('telescope.builtin').grep_string { search = vim.fn.expand("<cword>") }
+end)
+nnoremap("<leader>pb", function()
+    require('telescope.builtin').buffers()
+end)
+nnoremap("<leader>vh", function()
+    require('telescope.builtin').help_tags()
+end)
