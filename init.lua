31b642596c04f6ffc3b769059fc634c95b22029e
laomuon <laomuon@github.com>
2022-12-19 08:57:08 +0100

    Change to native LSP

--- init.lua
+++ init.lua
@@ -18,6 +18,9 @@ vim.opt.smartcase = true
 
 vim.opt.backup = false
 vim.opt.writebackup = false
+vim.opt.swapfile = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
 
 -- Having longer updatetime (default is 4000 ms = 4 s) leads to noticeable
 -- delays and poor user experience.
@@ -26,6 +29,8 @@ vim.opt.updatetime = 300
 -- Always show the signcolumn, otherwise it would shift the text each time
 -- diagnostics appear/become resolved.
 vim.opt.signcolumn = "yes"
+
+vim.opt.scrolloff = 8
 
 vim.cmd [[packadd packer.nvim]]
 
@@ -59,7 +64,6 @@ require('packer').startup(function(use)
     })
   use("junegunn/fzf")
   use("junegunn/fzf.vim")
-  use { 'neoclide/coc.nvim', branch='release' }
   use 'xiyaowong/nvim-transparent'
   use 'lewis6991/gitsigns.nvim'
   use {
@@ -67,6 +71,27 @@ require('packer').startup(function(use)
     requires = { 'nvim-tree/nvim-web-devicons' },
     }
   use 'numToStr/Comment.nvim'
+  use {
+  'VonHeikemen/lsp-zero.nvim',
+  requires = {
+    -- LSP Support
+    {'neovim/nvim-lspconfig'},
+    {'williamboman/mason.nvim'},
+    {'williamboman/mason-lspconfig.nvim'},
+
+    -- Autocompletion
+    {'hrsh7th/nvim-cmp'},
+    {'hrsh7th/cmp-buffer'},
+    {'hrsh7th/cmp-path'},
+    {'saadparwaiz1/cmp_luasnip'},
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'hrsh7th/cmp-nvim-lua'},
+
+    -- Snippets
+    {'L3MON4D3/LuaSnip'},
+    {'rafamadriz/friendly-snippets'},
+  }
+}
 end)
 -- Set colortheme
 vim.g.gruvbox_transparent_bg = '1'
@@ -210,9 +235,11 @@ local vnoremap = bind("v")
 local xnoremap = bind("x")
 local inoremap = bind("i")
 
+nnoremap("<leader>fp", ":echo @%<CR>")
 nnoremap("<leader>ft", ":NvimTreeToggle<CR>")
+nnoremap("<Esc>", ":noh<CR>")
 nnoremap("<C-p>", ":Telescope")
-nnoremap("<leader>fs", function()
+nnoremap("<leader>pw", function()
     require('telescope.builtin').grep_string({ search = vim.fn.input("Grep For > ")})
 end)
 nnoremap("<C-p>", function()
@@ -222,7 +249,7 @@ nnoremap("<Leader>ff", function()
     require('telescope.builtin').find_files()
 end)
 
-nnoremap("<leader>pw", function()
+nnoremap("<leader>fs", function()
     require('telescope.builtin').grep_string { search = vim.fn.expand("<cword>") }
 end)
 nnoremap("<leader>pb", function()
@@ -232,41 +259,57 @@ nnoremap("<leader>vh", function()
     require('telescope.builtin').help_tags()
 end)
 
+nnoremap("<C-d>", "<C-d>zz")
+nnoremap("<C-u>", "<C-u>zz")
+
 -- keymap for coc
-local keyset = vim.keymap.set
+-- local keyset = vim.keymap.set
 
-local opts = {silent = true, noremap = true, expr = true, replace_keycodes = false}
-keyset("i", "<cr>", [[coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"]], opts)
+-- local opts = {silent = true, noremap = true, expr = true, replace_keycodes = false}
+-- keyset("i", "<cr>", [[coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"]], opts)
+--
+-- -- GoTo code navigation.
+-- keyset("n", "gd", "<Plug>(coc-definition)", {silent = true})
+-- keyset("n", "gy", "<Plug>(coc-type-definition)", {silent = true})
+-- keyset("n", "gi", "<Plug>(coc-implementation)", {silent = true})
+-- keyset("n", "gr", "<Plug>(coc-references)", {silent = true})
+--
+-- -- Use K to show documentation in preview window.
+-- function _G.show_docs()
+--     local cw = vim.fn.expand('<cword>')
+--     if vim.fn.index({'vim', 'help'}, vim.bo.filetype) >= 0 then
+--         vim.api.nvim_command('h ' .. cw)
+--     elseif vim.api.nvim_eval('coc#rpc#ready()') then
+--         vim.fn.CocActionAsync('doHover')
+--     else
+--         vim.api.nvim_command('!' .. vim.o.keywordprg .. ' ' .. cw)
+--     end
+-- end
+-- keyset("n", "K", '<CMD>lua _G.show_docs()<CR>', {silent = true})
+--
+--
+-- -- Highlight the symbol and its references when holding the cursor.
+-- vim.api.nvim_create_augroup("CocGroup", {})
+-- vim.api.nvim_create_autocmd("CursorHold", {
+--     group = "CocGroup",
+--     command = "silent call CocActionAsync('highlight')",
+--     desc = "Highlight symbol under cursor on CursorHold"
+-- })
+-- -- Remap <C-f> and <C-b> for scroll float windows/popups.
+-- ---@diagnostic disable-next-line: redefined-local
+-- local opts = {silent = true, nowait = true, expr = true}
+-- keyset("n", "<C-f>", 'coc#float#has_scroll() ? coc#float#scroll(1) : "<C-f>"', opts)
+-- keyset("n", "<C-b>", 'coc#float#has_scroll() ? coc#float#scroll(0) : "<C-b>"', opts)
 
--- GoTo code navigation.
-keyset("n", "gd", "<Plug>(coc-definition)", {silent = true})
-keyset("n", "gy", "<Plug>(coc-type-definition)", {silent = true})
-keyset("n", "gi", "<Plug>(coc-implementation)", {silent = true})
-keyset("n", "gr", "<Plug>(coc-references)", {silent = true})
+local lsp = require('lsp-zero')
 
--- Use K to show documentation in preview window.
-function _G.show_docs()
-    local cw = vim.fn.expand('<cword>')
-    if vim.fn.index({'vim', 'help'}, vim.bo.filetype) >= 0 then
-        vim.api.nvim_command('h ' .. cw)
-    elseif vim.api.nvim_eval('coc#rpc#ready()') then
-        vim.fn.CocActionAsync('doHover')
-    else
-        vim.api.nvim_command('!' .. vim.o.keywordprg .. ' ' .. cw)
-    end
-end
-keyset("n", "K", '<CMD>lua _G.show_docs()<CR>', {silent = true})
+lsp.preset('recommended')
+-- lsp.setup_nvim_cmp({
+--   sources = {
+--     {name = 'path'},
+--     {name = 'nvim_lsp', keyword_length = 3},
+--     {name = 'buffer', keyword_length = 3},
+--   }
+-- })
+lsp.setup()
 
-
--- Highlight the symbol and its references when holding the cursor.
-vim.api.nvim_create_augroup("CocGroup", {})
-vim.api.nvim_create_autocmd("CursorHold", {
-    group = "CocGroup",
-    command = "silent call CocActionAsync('highlight')",
-    desc = "Highlight symbol under cursor on CursorHold"
-})
--- Remap <C-f> and <C-b> for scroll float windows/popups.
----@diagnostic disable-next-line: redefined-local
-local opts = {silent = true, nowait = true, expr = true}
-keyset("n", "<C-f>", 'coc#float#has_scroll() ? coc#float#scroll(1) : "<C-f>"', opts)
-keyset("n", "<C-b>", 'coc#float#has_scroll() ? coc#float#scroll(0) : "<C-b>"', opts)
