4032a69063ed704c7e0e1d27daa03eaaca571d1d
laomuon <laomuon@github.com>
2023-09-20 22:09:59 +0200

    Clean up unused plugins

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
 --vim.opt.guicursor = ""
+local vim = vim
 vim.opt.termguicolors = true
 vim.opt.nu = true
 vim.opt.relativenumber = true
@@ -49,11 +50,6 @@ require('packer').startup(function(use)
   tag = 'nightly' -- optional, updated every week. (see issue #1193)
   }
 
-  -- Gruvbox colortheme
-  use 'morhetz/gruvbox'
-  use { "catppuccin/nvim", as = "catppuccin" }
-  use 'arcticicestudio/nord-vim'
- 
   use{
       'nvim-telescope/telescope.nvim',
       requires = {{'nvim-lua/plenary.nvim'} }
@@ -72,10 +68,6 @@ require('packer').startup(function(use)
   use 'lewis6991/gitsigns.nvim'
   use ({'rebelot/kanagawa.nvim', commit = 'fc2e308'})
   use 'lukas-reineke/indent-blankline.nvim'
-  use {
-    'goolord/alpha-nvim',
-    requires = { 'nvim-tree/nvim-web-devicons' },
-    }
     use 'numToStr/Comment.nvim'
     use {
   'VonHeikemen/lsp-zero.nvim',
@@ -101,62 +93,14 @@ require('packer').startup(function(use)
     {'jose-elias-alvarez/null-ls.nvim'},
     {'jay-babu/mason-null-ls.nvim'},
 
-    -- Markdown
-    use {'iamcco/markdown-preview.nvim'}
-  }
+  },
 }
+    use 'mbbill/undotree'
 end)
--- Set colortheme
-vim.g.gruvbox_transparent_bg = '1'
-vim.g.gruvbox_invert_selection = '0'
-vim.g.gruvbox_contrast_dark = 'hard'
 
 require("Comment").setup()
 
-require("catppuccin").setup({
-    flavour = "latte", -- latte, frappe, macchiato, mocha
-    background = { -- :h background
-        light = "latte",
-        dark = "macchiato",
-    },
-    transparent_background = true,
-    term_colors = true,
-    dim_inactive = {
-        enabled = false,
-        shade = "dark",
-        percentage = 0.15,
-    },
-    no_italic = false, -- Force no italic
-    no_bold = false, -- Force no bold
-    styles = {
-        comments = {},
-        conditionals = {},
-        loops = {},
-        functions = {},
-        keywords = {},
-        strings = {},
-        variables = {},
-        numbers = {},
-        booleans = {},
-        properties = {},
-        types = {},
-        operators = {},
-    },
-    color_overrides = {
-        mocha = {
-            base = "#000000",
-        },
-    },
-    custom_highlights = {},
-    integrations = {
-        cmp = true,
-        gitsigns = true,
-        nvimtree = true,
-        telescope = true,
-        notify = false,
-        mini = false,
-    },
-})
+require('kanagawa').setup({transparent=true,})
 vim.cmd("colorscheme kanagawa")
 
 require('indent_blankline').setup()
@@ -202,7 +146,7 @@ require('gitsigns').setup{
   end
 }
 require'nvim-treesitter.configs'.setup {
-    ensure_installed = {"c", "bash", "lua", "python", "vim", "help", "query", "cpp", "markdown"},
+    ensure_installed = {"c", "bash", "lua", "python", "vim", "query", "cpp", "markdown"},
     sync_install = false,
     indent = {
         enable = true,
@@ -210,6 +154,9 @@ require'nvim-treesitter.configs'.setup {
 
     highlight = {
         enable = true,
+        disable = function (lang, bufnr)
+            return vim.api.nvim_buf_line_count(bufnr) > 5000
+        end,
         additional_vim_regex_highlighting = false,
     },
 }
@@ -223,9 +170,17 @@ require("lualine").setup({
         theme = 'kanagawa',
     }
 })
+
 require('telescope').setup({
     defaults = {
         wrap_results = true,
+        mappings = {
+            i = {
+                ["<Esc>"] = require("telescope.actions").close,
+                ["<C-j>"] = require("telescope.actions").move_selection_next,
+                ["<C-k>"] = require("telescope.actions").move_selection_previous,
+            },
+        },
     },
 })
 require('nvim-tree').setup ({
@@ -245,7 +200,6 @@ require('nvim-tree').setup ({
     }
 })
 require("transparent").setup({
-  enable = true, -- boolean: enable transparent
   extra_groups = { -- table/string: additional groups that should be cleared
     -- In particular, when you set it to 'all', that means all available groups
 
@@ -257,109 +211,9 @@ require("transparent").setup({
     "BufferLineSeparator",
     "BufferLineIndicatorSelected"
   },
-  exclude = {}, -- table: groups you don't want to clear
+  exclude_groups = {}, -- table: groups you don't want to clear
 })
 
-local alpha = require("alpha")
-local dashboard = require("alpha.themes.dashboard")
-
--- Set header
-dashboard.section.header.val = {
-"                                                                                                ",
-              "                                                                                                ",
-              "                                                                                                ",
-              "                                                                                                ",
-              "                                                                                                ",
-              "         -╲         '-                                                                          ",
-              "       -' :╲        │ '-                                                                        ",
-              "     -'   : ╲       │   '-                          │MMM│                                       ",
-              "   -'·    :  ╲      │     '-                        │WWW│                                       ",
-              "  '.-.·   :   ╲     │       │                                                                   ",
-              "  │. .-·  :    ╲    │       │    MMM=         =MMM  │MMM│  │M│  +===+   +====+                  ",
-              "  │ . .-· :     ╲   │       │    ╲HHB`       'BHH╱  │HHH│  │H│╲╱sMMMs╲_╱sMMMMs╲                 ",
-              "  │. . .-·;      ╲  │       │     ╲HHH╲     ╱HHH╱   │HHH│  │HBBWWWWWHMMMHWWWW:B╲                ",
-              "  │ . . .-│       ╲ │       │      ╲HHH╲   ╱HHH╱    │HHH│  │HK╱     ╲KYK╱    ╲KH│               ",
-              "  │. . . .│╲       ╲│       │       ╲HHH╲ ╱HHH╱     │HHH│  │H│       │H│      │H│               ",
-              "  │ . . . │ ╲       ;-      │        ╲HHHVHHH╱      │HHH│  │H│       │H│      │H│               ",
-              "  │. . . .│  ╲      :·-     │         ╲HHHHH╱       │HHH│  │H│       │H│      │H│               ",
-              "  │ . . . │   ╲     : .-    │          ╲HHH╱        │HHH│  │H│       │H│      │H│               ",
-              "  │. . . .│    ╲    :. .-   │           ╲W╱         │WWW│  │W│       │W│      │W│               ",
-              "  `- . . .│     ╲   : . .- -'                                                                   ",
-              "    `- . .│      ╲  :. ..-'                                                                     ",
-              "      `-. │       ╲ :..-'                                                                       ",
-              "         `│        ╲;-'                                                                         ",
-              "                                                                                                ",
-              "                                                                                                ",
-}
-
--- Set menu
-dashboard.section.buttons.val = {
-    dashboard.button( "e", "  > New file" , ":ene <BAR> startinsert <CR>"),
-    dashboard.button( "f", "  > Find file", ":cd /home/muon/eureka | Telescope find_files<CR>"),
-    dashboard.button( "r", "  > Recent"   , ":Telescope oldfiles<CR>"),
-    dashboard.button( "s", "  > Settings" , ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
-    dashboard.button( "q", "  > Quit NVIM", ":qa<CR>"),
-}
-
--- Set footer
- local fortune = require("alpha.fortune") 
- dashboard.section.footer.val = fortune()
-
--- Send config to alpha
-alpha.setup(dashboard.opts)
-
-vim.cmd [[
-let g:mkdp_auto_start = 0
-
-let g:mkdp_auto_close = 1
-
-let g:mkdp_refresh_slow = 0
-
-let g:mkdp_command_for_global = 0
-
-let g:mkdp_open_to_the_world = 0
-
-let g:mkdp_open_ip = ''
-
-let g:mkdp_browser = 'firefox'
-
-let g:mkdp_echo_preview_url = 0
-  function OpenMarkdownPreview (url)
-    execute "silent ! firefox --new-window " . a:url
-  endfunction
-  let g:mkdp_browserfunc = 'OpenMarkdownPreview'
-
-let g:mkdp_preview_options = {
-    \ 'mkit': {},
-    \ 'katex': {},
-    \ 'uml': {},
-    \ 'maid': {},
-    \ 'disable_sync_scroll': 0,
-    \ 'sync_scroll_type': 'middle',
-    \ 'hide_yaml_meta': 1,
-    \ 'sequence_diagrams': {},
-    \ 'flowchart_diagrams': {},
-    \ 'content_editable': v:false,
-    \ 'disable_filename': 0,
-    \ 'toc': {}
-    \ }
-
-let g:mkdp_markdown_css = 'expand("~/github-markdown.css")'
-
-let g:mkdp_highlight_css = 'expand("~/highlight.css")'
-
-let g:mkdp_port = ''
-
-let g:mkdp_page_title = '「${name}」'
-
-let g:mkdp_filetypes = ['markdown']
-
-let g:mkdp_theme = 'dark'
-]]
--- Disable folding on alpha buffer
-vim.cmd([[
-    autocmd FileType alpha setlocal nofoldenable
-]])
 vim.g.mapleader = " "
 
 local function bind(op, outer_opts)
@@ -382,12 +236,8 @@ local inoremap = bind("i")
 nnoremap("<leader>fp", ":echo @%<CR>")
 nnoremap("<leader>ft", ":NvimTreeFindFileToggle<CR>")
 nnoremap("<Esc>", ":noh<CR>")
-nnoremap("<C-p>", ":Telescope")
 nnoremap("<leader>pw", function()
-    require('telescope.builtin').grep_string({ search = vim.fn.input("Grep For > ")})
-end)
-nnoremap("<C-p>", function()
-    require('telescope.builtin').git_files()
+    require('telescope.builtin').grep_string{ search = vim.fn.input("Grep For > ")}
 end)
 nnoremap("<Leader>ff", function()
     require('telescope.builtin').find_files()
@@ -402,25 +252,30 @@ end)
 nnoremap("<leader>vh", function()
     require('telescope.builtin').help_tags()
 end)
+nnoremap("gr", function()
+    require('telescope.builtin').lsp_references()
+end)
+nnoremap("gd", function()
+    require('telescope.builtin').lsp_definitions()
+end)
+nnoremap("<leader>sd", function()
+    require('telescope.builtin').diagnostics({bufnr=0})
+end)
+nnoremap("<leader><F5>", vim.cmd.UndotreeToggle)
 
 nnoremap("<C-d>", "<C-d>zz")
 nnoremap("<C-u>", "<C-u>zz")
 nnoremap("<C-w>v", "<C-w>v<C-w>l")
 nnoremap("<C-w>s", "<C-w>s<C-w>j")
-nnoremap("<leader>mp", ":MarkdownPreviewToggle<CR>")
 
 
 local lsp = require('lsp-zero')
 
-lsp.preset('recommended')
+lsp.preset({
+    name='recommended',
+    set_lsp_keymaps={preserve_mappings=true, omit={"gd","gr", "C-p"}}
+})
 local cmp = require('cmp')
-local cmp_mappings = lsp.defaults.cmp_mappings({
-    ['<C-j>'] = cmp.mapping.select_next_item(),
-    ['<C-k>'] = cmp.mapping.select_prev_item(),
-})
-lsp.setup_nvim_cmp({
-    mapping = cmp_mappings
-})
 lsp.setup_nvim_cmp({
   sources = {
     {name = 'path'},
@@ -429,7 +284,36 @@ lsp.setup_nvim_cmp({
     {name = 'luasnip', keyword_length = 2},
   }
 })
-lsp.setup()
+vim.o.foldcolumn = '1'
+vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
+
+lsp.on_attach(function(client, bufnr)
+  lsp.default_keymaps({buffer = bufnr})
+end)
+
+lsp.set_server_config({
+  capabilities = {
+    textDocument = {
+      foldingRange = {
+        dynamicRegistration = false,
+        lineFoldingOnly = true
+      }
+    }
+  }
+})
+
+lsp.setup({})
+cmp.setup({
+  mapping = {
+    -- `Enter` key to confirm completion
+    ['<CR>'] = cmp.mapping.confirm({select = true}),
+    ['<C-j>'] = cmp.mapping.select_next_item(),
+    ['<C-k>'] = cmp.mapping.select_prev_item(),
+    }
+})
+
 null_ls = require("null-ls")
 lsp_config = require("lspconfig")
 local code_actions = null_ls.builtins.code_actions
@@ -442,7 +326,6 @@ mason_null.setup({
         automatic_setup = true,
 
     })
-mason_null.setup_handlers({})
 vim.diagnostic.config({
   virtual_text = true,
   signs = true,
@@ -451,6 +334,3 @@ vim.diagnostic.config({
   severity_sort = false,
   float = {border = "single"},
 })
--- vim.opt.foldlevel = 20
--- vim.opt.foldmethod = "expr"
--- vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -49,8 +49,8 @@ local function save_profiles(threshold)
 end
 
 time([[Luarocks path setup]], true)
-local package_path_str = "/home/muon/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?.lua;/home/muon/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?/init.lua;/home/muon/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?.lua;/home/muon/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?/init.lua"
-local install_cpath_pattern = "/home/muon/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/lua/5.1/?.so"
+local package_path_str = "/home/ci/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?.lua;/home/ci/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?/init.lua;/home/ci/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?.lua;/home/ci/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/home/ci/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/lua/5.1/?.so"
 if not string.find(package.path, package_path_str, 1, true) then
   package.path = package.path .. ';' .. package_path_str
 end
@@ -76,173 +76,183 @@ time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
   ["Comment.nvim"] = {
     loaded = true,
-    path = "/home/muon/.local/share/nvim/site/pack/packer/start/Comment.nvim",
+    path = "/home/ci/.local/share/nvim/site/pack/packer/start/Comment.nvim",
     url = "https://github.com/numToStr/Comment.nvim"
   },
   LuaSnip = {
     loaded = true,
-    path = "/home/muon/.local/share/nvim/site/pack/packer/start/LuaSnip",
+    path = "/home/ci/.local/share/nvim/site/pack/packer/start/LuaSnip",
     url = "https://github.com/L3MON4D3/LuaSnip"
   },
   ["alpha-nvim"] = {
     loaded = true,
-    path = "/home/muon/.local/share/nvim/site/pack/packer/start/alpha-nvim",
+    path = "/home/ci/.local/share/nvim/site/pack/packer/start/alpha-nvim",
     url = "https://github.com/goolord/alpha-nvim"
   },
   catppuccin = {
     loaded = true,
-    path = "/home/muon/.local/share/nvim/site/pack/packer/start/catppuccin",
+    path = "/home/ci/.local/share/nvim/site/pack/packer/start/catppuccin",
     url = "https://github.com/catppuccin/nvim"
   },
   ["cmp-buffer"] = {
     loaded = true,
-    path = "/home/muon/.local/share/nvim/site/pack/packer/start/cmp-buffer",
+    path = "/home/ci/.local/share/nvim/site/pack/packer/start/cmp-buffer",
     url = "https://github.com/hrsh7th/cmp-buffer"
   },
   ["cmp-nvim-lsp"] = {
     loaded = true,
-    path = "/home/muon/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
+    path = "/home/ci/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
     url = "https://github.com/hrsh7th/cmp-nvim-lsp"
   },
   ["cmp-nvim-lua"] = {
     loaded = true,
-    path = "/home/muon/.local/share/nvim/site/pack/packer/start/cmp-nvim-lua",
+    path = "/home/ci/.local/share/nvim/site/pack/packer/start/cmp-nvim-lua",
     url = "https://github.com/hrsh7th/cmp-nvim-lua"
   },
   ["cmp-path"] = {
     loaded = true,
-    path = "/home/muon/.local/share/nvim/site/pack/packer/start/cmp-path",
+    path = "/home/ci/.local/share/nvim/site/pack/packer/start/cmp-path",
     url = "https://github.com/hrsh7th/cmp-path"
   },
   cmp_luasnip = {
     loaded = true,
-    path = "/home/muon/.local/share/nvim/site/pack/packer/start/cmp_luasnip",
+    path = "/home/ci/.local/share/nvim/site/pack/packer/start/cmp_luasnip",
     url = "https://github.com/saadparwaiz1/cmp_luasnip"
   },
   ["friendly-snippets"] = {
     loaded = true,
-    path = "/home/muon/.local/share/nvim/site/pack/packer/start/friendly-snippets",
+    path = "/home/ci/.local/share/nvim/site/pack/packer/start/friendly-snippets",
     url = "https://github.com/rafamadriz/friendly-snippets"
   },
   fzf = {
     loaded = true,
-    path = "/home/muon/.local/share/nvim/site/pack/packer/start/fzf",
+    path = "/home/ci/.local/share/nvim/site/pack/packer/start/fzf",
     url = "https://github.com/junegunn/fzf"
   },
   ["fzf.vim"] = {
     loaded = true,
-    path = "/home/muon/.local/share/nvim/site/pack/packer/start/fzf.vim",
+    path = "/home/ci/.local/share/nvim/site/pack/packer/start/fzf.vim",
     url = "https://github.com/junegunn/fzf.vim"
   },
   ["gitsigns.nvim"] = {
     loaded = true,
-    path = "/home/muon/.local/share/nvim/site/pack/packer/start/gitsigns.nvim",
+    path = "/home/ci/.local/share/nvim/site/pack/packer/start/gitsigns.nvim",
     url = "https://github.com/lewis6991/gitsigns.nvim"
   },
   gruvbox = {
     loaded = true,
-    path = "/home/muon/.local/share/nvim/site/pack/packer/start/gruvbox",
+    path = "/home/ci/.local/share/nvim/site/pack/packer/start/gruvbox",
     url = "https://github.com/morhetz/gruvbox"
   },
   ["indent-blankline.nvim"] = {
     loaded = true,
-    path = "/home/muon/.local/share/nvim/site/pack/packer/start/indent-blankline.nvim",
+    path = "/home/ci/.local/share/nvim/site/pack/packer/start/indent-blankline.nvim",
     url = "https://github.com/lukas-reineke/indent-blankline.nvim"
   },
   ["kanagawa.nvim"] = {
     loaded = true,
-    path = "/home/muon/.local/share/nvim/site/pack/packer/start/kanagawa.nvim",
+    path = "/home/ci/.local/share/nvim/site/pack/packer/start/kanagawa.nvim",
     url = "https://github.com/rebelot/kanagawa.nvim"
   },
   ["lsp-zero.nvim"] = {
     loaded = true,
-    path = "/home/muon/.local/share/nvim/site/pack/packer/start/lsp-zero.nvim",
+    path = "/home/ci/.local/share/nvim/site/pack/packer/start/lsp-zero.nvim",
     url = "https://github.com/VonHeikemen/lsp-zero.nvim"
   },
   ["lualine.nvim"] = {
     loaded = true,
-    path = "/home/muon/.local/share/nvim/site/pack/packer/start/lualine.nvim",
+    path = "/home/ci/.local/share/nvim/site/pack/packer/start/lualine.nvim",
     url = "https://github.com/nvim-lualine/lualine.nvim"
-  },
-  ["markdown-preview.nvim"] = {
-    loaded = true,
-    path = "/home/muon/.local/share/nvim/site/pack/packer/start/markdown-preview.nvim",
-    url = "https://github.com/iamcco/markdown-preview.nvim"
   },
   ["mason-lspconfig.nvim"] = {
     loaded = true,
-    path = "/home/muon/.local/share/nvim/site/pack/packer/start/mason-lspconfig.nvim",
+    path = "/home/ci/.local/share/nvim/site/pack/packer/start/mason-lspconfig.nvim",
     url = "https://github.com/williamboman/mason-lspconfig.nvim"
   },
   ["mason-null-ls.nvim"] = {
     loaded = true,
-    path = "/home/muon/.local/share/nvim/site/pack/packer/start/mason-null-ls.nvim",
+    path = "/home/ci/.local/share/nvim/site/pack/packer/start/mason-null-ls.nvim",
     url = "https://github.com/jay-babu/mason-null-ls.nvim"
   },
   ["mason.nvim"] = {
     loaded = true,
-    path = "/home/muon/.local/share/nvim/site/pack/packer/start/mason.nvim",
+    path = "/home/ci/.local/share/nvim/site/pack/packer/start/mason.nvim",
     url = "https://github.com/williamboman/mason.nvim"
   },
   ["nord-vim"] = {
     loaded = true,
-    path = "/home/muon/.local/share/nvim/site/pack/packer/start/nord-vim",
+    path = "/home/ci/.local/share/nvim/site/pack/packer/start/nord-vim",
     url = "https://github.com/arcticicestudio/nord-vim"
   },
   ["null-ls.nvim"] = {
     loaded = true,
-    path = "/home/muon/.local/share/nvim/site/pack/packer/start/null-ls.nvim",
+    path = "/home/ci/.local/share/nvim/site/pack/packer/start/null-ls.nvim",
     url = "https://github.com/jose-elias-alvarez/null-ls.nvim"
   },
   ["nvim-cmp"] = {
     loaded = true,
-    path = "/home/muon/.local/share/nvim/site/pack/packer/start/nvim-cmp",
+    path = "/home/ci/.local/share/nvim/site/pack/packer/start/nvim-cmp",
     url = "https://github.com/hrsh7th/nvim-cmp"
   },
   ["nvim-lspconfig"] = {
     loaded = true,
-    path = "/home/muon/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
+    path = "/home/ci/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
     url = "https://github.com/neovim/nvim-lspconfig"
   },
   ["nvim-transparent"] = {
     loaded = true,
-    path = "/home/muon/.local/share/nvim/site/pack/packer/start/nvim-transparent",
+    path = "/home/ci/.local/share/nvim/site/pack/packer/start/nvim-transparent",
     url = "https://github.com/xiyaowong/nvim-transparent"
   },
   ["nvim-tree.lua"] = {
     loaded = true,
-    path = "/home/muon/.local/share/nvim/site/pack/packer/start/nvim-tree.lua",
+    path = "/home/ci/.local/share/nvim/site/pack/packer/start/nvim-tree.lua",
     url = "https://github.com/nvim-tree/nvim-tree.lua"
   },
   ["nvim-treesitter"] = {
     loaded = true,
-    path = "/home/muon/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
+    path = "/home/ci/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
     url = "https://github.com/nvim-treesitter/nvim-treesitter"
   },
   ["nvim-treesitter-context"] = {
     loaded = true,
-    path = "/home/muon/.local/share/nvim/site/pack/packer/start/nvim-treesitter-context",
+    path = "/home/ci/.local/share/nvim/site/pack/packer/start/nvim-treesitter-context",
     url = "https://github.com/nvim-treesitter/nvim-treesitter-context"
+  },
+  ["nvim-ufo"] = {
+    loaded = true,
+    path = "/home/ci/.local/share/nvim/site/pack/packer/start/nvim-ufo",
+    url = "https://github.com/kevinhwang91/nvim-ufo"
   },
   ["nvim-web-devicons"] = {
     loaded = true,
-    path = "/home/muon/.local/share/nvim/site/pack/packer/start/nvim-web-devicons",
+    path = "/home/ci/.local/share/nvim/site/pack/packer/start/nvim-web-devicons",
     url = "https://github.com/nvim-tree/nvim-web-devicons"
   },
   ["packer.nvim"] = {
     loaded = true,
-    path = "/home/muon/.local/share/nvim/site/pack/packer/start/packer.nvim",
+    path = "/home/ci/.local/share/nvim/site/pack/packer/start/packer.nvim",
     url = "https://github.com/wbthomason/packer.nvim"
   },
   ["plenary.nvim"] = {
     loaded = true,
-    path = "/home/muon/.local/share/nvim/site/pack/packer/start/plenary.nvim",
+    path = "/home/ci/.local/share/nvim/site/pack/packer/start/plenary.nvim",
     url = "https://github.com/nvim-lua/plenary.nvim"
+  },
+  ["promise-async"] = {
+    loaded = true,
+    path = "/home/ci/.local/share/nvim/site/pack/packer/start/promise-async",
+    url = "https://github.com/kevinhwang91/promise-async"
   },
   ["telescope.nvim"] = {
     loaded = true,
-    path = "/home/muon/.local/share/nvim/site/pack/packer/start/telescope.nvim",
+    path = "/home/ci/.local/share/nvim/site/pack/packer/start/telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim"
+  },
+  undotree = {
+    loaded = true,
+    path = "/home/ci/.local/share/nvim/site/pack/packer/start/undotree",
+    url = "https://github.com/mbbill/undotree"
   }
 }
 
