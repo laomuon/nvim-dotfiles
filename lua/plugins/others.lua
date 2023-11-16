772d4bd9688a9390628b25b63de8a5431160f0d0
laomuon <laomuon@github.com>
2023-11-16 12:10:04 +0100

    Remove undotree

--- lua/plugins/others.lua
+++ lua/plugins/others.lua
@@ -1,10 +1,4 @@
 return {
     {"lukas-reineke/indent-blankline.nvim", main="ibl", opts={scope = {enabled=false}}},
     {"numToStr/Comment.nvim", lazy=false, opts={}},
-    {
-        'mbbill/undotree',
-        init = function()
-            vim.keymap.set("n", "<leader><F5>", vim.cmd.UndotreeToggle)
-        end
-    }
 }
